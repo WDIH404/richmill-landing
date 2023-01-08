fe47328d43c42e18a5dc03b76505683e905e06b5
WDIH404 <flazy322@gmail.com>
2023-01-08 15:51:18 +0700

    +

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(function () {
   $('.counter').countimator();
 });
+
+function sendMail() {
+  var params = {
+    name: document.getElementById('name').value,
+    phone: document.getElementById('phone').value,
+  };
+  const serviceID = '';
+  const templateID = '';
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById('name').value = '';
+      document.getElementById('phone').value = '';
+      console.log(res);
+      alert('Готово!');
+    })
+    .catch((err) => console.log(err));
+}
